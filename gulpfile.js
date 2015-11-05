9b7252207d4f8730a2485a7de2ce504a9b15b895
gurumohanreddy <gurumohan449@gmail.com>
2015-11-06 00:13:30 +0530

    Initial design

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,15 @@
-gulpfile.js
+var gulp = require('gulp'),
+ sass = require('gulp-sass');
+
+
+ gulp.task('scss', function () {
+  gulp.src('./scss/src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/src/*.scss', ['scss']);
+});
+
+gulp.task('default', ['sass:watch']);
