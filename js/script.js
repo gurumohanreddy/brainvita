14f80b40d3c645bb0836f273f77eacd122a900a5
gurumohanreddy <gurumohan449@gmail.com>
2015-11-06 19:21:54 +0530

    Logic1 completed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@
 
   function brainvita(){
     var movable=0;
-    var idValues=0;
+    var rows =null;
+    var Column = null;
+    var emptyMarble = null;
+    var emptyRows = null;
+    var emptyColumn = null;
+    var passingEmpty =null;
     refreshListeners();
     function refreshListeners(){
       $('.empty').off('click');
@@ -15,26 +20,51 @@
 
     function handleMarbleClicks(){
         movable = $(this);
-        idValues=$(this).attr('id').split('.');
-        console.log(Number(idValues[1])+2);
+        var idValues=$(this).attr('id').split('.');
+        rows = Number(idValues[0]);
+        column = Number(idValues[1]);
+
         //refreshListeners();
     }
     function handleEmptyClicks(){
+
       if (movable === 0){
           return;
       }
-      // else if (Number(idValues[1])+2 >= 7 && Number(idValues[1])-2 >= 0) {
+      passingEmpty = $(this)
+      var emptyMarble = $(passingEmpty).attr('id').split('.');
+      emptyRows = Number(emptyMarble[0]);
+      emptyColumn = Number(emptyMarble[1]);
 
-      $(this).addClass('marble').addClass('slot').removeClass('empty');
+      if (rows === emptyRows) {
+          if (Math.abs(column-emptyColumn)===2) {
+            replacable();
+          }
+          else {
+            return;
+          }
+      }
+      else if (column = emptyColumn) {
+        if (Math.abs(rows-emptyRows)===2) {
+              replacable();
+        }
+        else {
+          return;
+        }
+      }
+
+      // $(this).addClass('marble').addClass('slot').removeClass('empty');
+      // $(movable).removeClass('marble').removeClass('slot').addClass('empty');
+      // refreshListeners();
+      // movable=0;
+
+    }
+
+    var replacable = function(){
+      $(passingEmpty).addClass('marble').addClass('slot').removeClass('empty');
       $(movable).removeClass('marble').removeClass('slot').addClass('empty');
-      console.log(movable);
       refreshListeners();
       movable=0;
-
-      // }
-      // else{
-      //   return;
-      // }
     }
 
 }
