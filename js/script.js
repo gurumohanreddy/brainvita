86d3316867a259f8f53715bb8a92165aff78cc86
gurumohanreddy <gurumohan449@gmail.com>
2015-11-06 18:27:39 +0530

    Basic Logic completed

--- js/script.js
+++ js/script.js
@@ -1,11 +1,40 @@
-$(document).ready(function(){
-    $('.marble').click(function(){
+  $(document).ready(function(){
+    brainvita();
+  });
 
-          console.log("clicked");
-            emptySlot();
-        });
+  function brainvita(){
+    var movable=0;
+    var idValues=0;
+    refreshListeners();
+    function refreshListeners(){
+      $('.empty').off('click');
+      $('.empty').on('click',handleEmptyClicks);
+      $('.marble').off('click');
+      $('.marble').on('click',handleMarbleClicks);
+    }
 
-        emptySlot = function(){
-          $()
-        }
-});
+    function handleMarbleClicks(){
+        movable = $(this);
+        idValues=$(this).attr('id').split('.');
+        console.log(Number(idValues[1])+2);
+        //refreshListeners();
+    }
+    function handleEmptyClicks(){
+      if (movable === 0){
+          return;
+      }
+      // else if (Number(idValues[1])+2 >= 7 && Number(idValues[1])-2 >= 0) {
+
+      $(this).addClass('marble').addClass('slot').removeClass('empty');
+      $(movable).removeClass('marble').removeClass('slot').addClass('empty');
+      console.log(movable);
+      refreshListeners();
+      movable=0;
+
+      // }
+      // else{
+      //   return;
+      // }
+    }
+
+}
